b8868c3518f29de2b9af3c190331d31640e4e0d1
cmb69 <cmbecker69@gmx.de>
2012-06-16 15:34:50 +0000

    added Danish translation (contributed by maeg)

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
 }
 
 
-define('REGISTER_VERSION', '1.4rc1');
+define('REGISTER_VERSION', '1.4rc2');
 
 
 /****************************************************************************
---
+++ languages/da.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Jens Maegaard - JmNet.dk - CMSimple-xh.dk
+ *
+ * This work is licensed under a Creative Commons Attribution 3.0 Unported
+ * License.
+ */
+
+$plugin_tx['register']['groupname']="Gruppenavn";
+$plugin_tx['register']['name']="Navn";
+$plugin_tx['register']['username']="Brugernavn";
+$plugin_tx['register']['oldpassword']="Gammel adgangskode";
+$plugin_tx['register']['password']="Adgangskode";
+$plugin_tx['register']['password2']="Bekræft adgangskode";
+$plugin_tx['register']['email']="Email";
+$plugin_tx['register']['code']="bekræftelseskode";
+$plugin_tx['register']['accessgroups']="Access Groups";
+$plugin_tx['register']['status']="Status";
+$plugin_tx['register']['register']="Register";
+$plugin_tx['register']['forgot_password']="Glemt kodeord";
+$plugin_tx['register']['user_prefs']="Bruger egenskaber";
+$plugin_tx['register']['user_delete']="Slet bruger";
+$plugin_tx['register']['user_deleted']="Bruger slettet";
+$plugin_tx['register']['user_locked']="Bruger egenskaber kan ikke ændres for";
+$plugin_tx['register']['admin_mode']="Administration Mode";
+$plugin_tx['register']['send']="Send";
+$plugin_tx['register']['change']="Skift";
+$plugin_tx['register']['fromip']="Fra IP-Adresse";
+$plugin_tx['register']['remember']="Remember&nbsp;User";
+$plugin_tx['register']['login']="Login";
+$plugin_tx['register']['logout']="Logud";
+$plugin_tx['register']['loggedin']="Logget ind";
+$plugin_tx['register']['loggedin_welcometext']="Hallo";
+$plugin_tx['register']['loggedout']="logget ud";
+$plugin_tx['register']['loggedout_text']="Du har nu logget ud.";
+$plugin_tx['register']['login_error']="Login fejl";
+$plugin_tx['register']['login_error_text']="Du har indtastet et forkert brugernavn eller adgangskode, eller din konto stadig ikke er aktiveret.";
+$plugin_tx['register']['access_error']="Adgangen er begrænset";
+$plugin_tx['register']['access_error_text']="Denne side er kun tilgængelig for medlemmer med godkendt tilladelser.";
+$plugin_tx['register']['error']="Fejl";
+$plugin_tx['register']['error_in_user']="Fejl i Brugernavn";
+$plugin_tx['register']['err_name']="Indtast venligst fornavn og efternavn.";
+$plugin_tx['register']['err_username']="Vælg venligst et brugernavn.";
+$plugin_tx['register']['err_username_illegal']="The username must contain only following characters: A-Z, a-z, 0-9, _";
+$plugin_tx['register']['err_username_exists']="Det valgte brugernavn findes allerede.";
+$plugin_tx['register']['err_username_does_not_exist']="Det valgte brugernaven findes ikke.";
+$plugin_tx['register']['err_username_notfound']="Brugernavnet findes ikke: ";
+$plugin_tx['register']['err_password']="Vælg venligst et kodeord.";
+$plugin_tx['register']['err_password2']="De to indtastede kodeors er ikke ens.";
+$plugin_tx['register']['err_password_illegal']="Adgangskoden må kun indeholde følgende tegn: A-Z, a-z, 0-9, _";
+$plugin_tx['register']['err_old_password_wrong']="Den gamle adgangskode du indtastede er forkert.";
+$plugin_tx['register']['err_email']="Indtast venligst din e-mail-adresse.";
+$plugin_tx['register']['err_email_invalid']="Den angivne e-mail adresse er ugyldig.";
+$plugin_tx['register']['err_email_exists']="En bruger med den angivne email-adressen eksisterer allerede.";
+$plugin_tx['register']['err_email_does_not_exist']="En bruger med den angivne e-mail-adresse eksisterer ikke.";
+$plugin_tx['register']['err_validation']="Bekræftelse kodenord forkert.";
+$plugin_tx['register']['err_colon']="må ikke indeholde et kolon.";
+$plugin_tx['register']['err_group_does_not_exist']="Gruppe findes ikke.";
+$plugin_tx['register']['err_csv_missing']="The CSV file does not exist.";
+$plugin_tx['register']['err_cannot_write_csv']="Saving CSV file failed.";
+$plugin_tx['register']['err_status_empty']="Status for dit brugernavn er tom.";
+$plugin_tx['register']['err_status_invalid']="Den indtastede bekræftelseskode er ugyldig.";
+$plugin_tx['register']['err_group_illegal']="Gruppen Navnet må kun indeholde følgende tegn: A-Z, a-z, 0-9, '_', '-'.";
+$plugin_tx['register']['registered']="Du er blevet registreret korrekt. En e-mail er blevet sendt til dig med et link til at aktivere din nye konto.";
+$plugin_tx['register']['register_form1']="Nye medlemmer for status &quot;Guest&quot; og kan ikke se mere end før. Statusen kan kun ændres af admin.";
+$plugin_tx['register']['register_form2']="Registreringen har måske brug for nogle sekunder, skal du vente til du modtager besked om at registreringen er en succes .";
+$plugin_tx['register']['activated']="Du har aktiveret din nye konto.";
+$plugin_tx['register']['emailsubject']="Konto aktivering for\r\n";
+$plugin_tx['register']['emailtext1']="En bruger-konto med din e-mail er blevet registreret med følgende oplysninger:";
+$plugin_tx['register']['emailtext2']="Klik nedenstående link for at aktivere din ny brugerkonto:";
+$plugin_tx['register']['reminderexplanation']="På denne side kan du indtaste din email adresse for at modtage en mail med dine kontoindstillinger.";
+$plugin_tx['register']['reminderemailsubject']="Konto data for\r\n";
+$plugin_tx['register']['remindersent']="Der er afsendten e-mail med dine bruger data";
+$plugin_tx['register']['changeexplanation']="På denne side kan du ændre dine kontoindstillinger. For at ændre dine data, skal du bruge din gamle adgangskode.";
+$plugin_tx['register']['prefsemailsubject']="Kontodata ændret for\r\n";
+$plugin_tx['register']['prefsupdated']="Din konto information er blevet opdateret og sendt til dig via e-mail.";
+$plugin_tx['register']['emailprefsupdated']="Dine kontooplysninger er blevet opdateret som følger.";
+$plugin_tx['register']['mnu_main']="Bruer/gruppe Administration";
+$plugin_tx['register']['mnu_config']="Plugin Konfiguration";
+$plugin_tx['register']['mnu_language']="Plugin sproginstillinger";
+$plugin_tx['register']['mnu_group_admin']="Gruppe Administration";
+$plugin_tx['register']['mnu_user_admin']="Bruger administration";
+$plugin_tx['register']['entries_in_csv']="Entries in CSV file ";
+$plugin_tx['register']['csv_written']="CSV file successfully written";
+$plugin_tx['register']['config_login_page']="";
+$plugin_tx['register']['config_usersfile']="plugins/register/data/users.csv";
+$plugin_tx['register']['config_groupsfile']="plugins/register/data/groups.csv";
+$plugin_tx['register']['cf_allowed_register']="<p><b>true</b> tillad registrering af nye medlemmer.</b></p>";
+$plugin_tx['register']['cf_captcha_mode']="<p><b>none</b> deaktivere captcha.</b></p><p><b>formula</b> aktiver en formular baseret captcha.</b></p><p><b>image</b> aktiver en billede baseret  captcha.</b></p>";
+$plugin_tx['register']['cf_encrypt_password']="<p><b>true</b> Aktiver kodeords kryptering.</b></p>";
+$plugin_tx['register']['cf_group_activated']="<p>Status for nye medlemmer (gruppe efter registrering og konto aktivering).</p>";
+$plugin_tx['register']['cf_group_administrator']="<p>Gruppe med admin rettigheder</p>";
+$plugin_tx['register']['cf_group_default']="<p>Status for nye medlemmer (standard gruppe for brugeren efter registrering, men før konto aktivering).</p>";
+$plugin_tx['register']['cf_hide_pages']="<p><b>true</b> Skjuler siderne i menuen som brugeren ikke har adgang til.</p>";
+$plugin_tx['register']['cf_logfile']="<p><b>true</b> aktiver logging af log-in og log-out af bruger i en log fil</p>";
+$plugin_tx['register']['cf_login_all_subsites']="<p><b>true</b>tillader login til alle andet sprog og underordnede websteder i én arbejdsgang.</p>";
+$plugin_tx['register']['cf_login_layout']="<p><b>horizontal</b> skaber en horisontal login-formular, tom eller alle andret skaber en lodret login form.</p>";
+$plugin_tx['register']['cf_remember_user']="<p><b>true</b> viser Husk brugeren afkrydsningsfeltet under login form.</p>";
+$plugin_tx['register']['cf_senderemail']="<p>E-mail-konto til administrator</p>";
+$plugin_tx['register']['cf_config_login_page']="Navn på den side brugeren omdirigeres direkte efter login.";
+$plugin_tx['register']['cf_config_usersfile']="Sti (til CMSimple root)til bruger CSV file, e.g. \"plugins/register/data/users.csv\".";
+$plugin_tx['register']['cf_config_groupsfile']="Sti (til CMSimple root) til gruppe CSV file, e.g. \"plugins/register/data/groups.csv\".";
+
+?>
