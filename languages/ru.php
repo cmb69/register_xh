94d31310aefe0e58f20fabd7947b8cac9b7e1f72
cmb69 <cmbecker69@gmx.de>
2012-09-07 09:50:27 +0000

    added Russian translation (contributed by Old)

---
+++ languages/ru.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * Copyright (c) 2012 Old aka Lybomyr Kydray
+ *
+ * Translation are licensed under the GNU General Public License, version 2 or
+ * later.
+ */
+
+/* added*/
+$plugin_tx['register']['menu_css']="Стили";
+$plugin_tx['register']['menu_config']="Конфигурация";
+$plugin_tx['register']['menu_language']="Язык";
+$plugin_tx['register']['menu_help']="Помощь";
+/* /added*/
+$plugin_tx['register']['groupname']="Название группы";
+$plugin_tx['register']['name']="Полное имя";
+$plugin_tx['register']['username']="Имя пользователя";
+$plugin_tx['register']['oldpassword']="Старый пароль";
+$plugin_tx['register']['password']="Пароль";
+$plugin_tx['register']['password2']="Подтверждение пароля";
+$plugin_tx['register']['email']="Электронная почта";
+$plugin_tx['register']['code']="Подтверждение кода";
+$plugin_tx['register']['accessgroups']="Группы доступа";
+$plugin_tx['register']['status']="Статус";
+$plugin_tx['register']['register']="Регистрация";
+$plugin_tx['register']['forgot_password']="Напомнить пароль";
+$plugin_tx['register']['user_prefs']="Данные пользователя";
+$plugin_tx['register']['user_delete']="Удалить пользователя";
+$plugin_tx['register']['user_deleted']="Пользователь удален";
+$plugin_tx['register']['user_locked']="Невозможноно изменить данные пользователя";
+$plugin_tx['register']['admin_mode']="Режим администрирования";
+$plugin_tx['register']['send']="Отослать";
+$plugin_tx['register']['change']="Изменить";
+$plugin_tx['register']['fromip']="Адрес IP";
+$plugin_tx['register']['remember']="Запомнить";
+$plugin_tx['register']['login']="Вход";
+$plugin_tx['register']['logout']="Выход";
+$plugin_tx['register']['loggedin']="вы вошли.";
+$plugin_tx['register']['loggedin_welcometext']="Хорошего настроения,<br />";
+$plugin_tx['register']['loggedout']="Выход произведен";
+$plugin_tx['register']['loggedout_text']="Вы вышли.";
+$plugin_tx['register']['login_error']="Ошибка авторизации";
+$plugin_tx['register']['login_error_text']="Вы указали неправильное имя пользователя или пароль, или ваша учетная запись еще не активирована.";
+$plugin_tx['register']['access_error']="Доступ ограничен";
+$plugin_tx['register']['access_error_text']="Эта страница доступна только пользователям с соответствующими правами.";
+$plugin_tx['register']['error']="Ошибка";
+$plugin_tx['register']['error_in_user']="Неправильное имя пользователя ";
+$plugin_tx['register']['err_name']="Введите, пожалуйста, имя и фамилию.";
+$plugin_tx['register']['err_username']="Пожалуйста, укажите пользователя.";
+$plugin_tx['register']['err_username_illegal']="Имя пользователя должно содержать только символы: <b>A-Z</b>, <b>a-z</b>, <b>0-9</b>, <b>_</b> .";
+$plugin_tx['register']['err_username_exists']="Пользователь с указанным именем уже существует.";
+$plugin_tx['register']['err_username_does_not_exist']="Пользователь с указанным именем не зарегистрирован.";
+$plugin_tx['register']['err_username_notfound']="Пользователь не найден: ";
+$plugin_tx['register']['err_password']="Пожалуйста, выберите пароль.";
+$plugin_tx['register']['err_password2']="Пароли не совпадают.";
+$plugin_tx['register']['err_password_illegal']="Пароль должен содержать только символы: <b>A-Z</b>, <b>a-z</b>, <b>0-9</b>, <b>_</b> .";
+$plugin_tx['register']['err_old_password_wrong']="Вы указали неправильный старый пароль.";
+$plugin_tx['register']['err_email']="Пожалуйста, укажите адрес электронной почты.";
+$plugin_tx['register']['err_email_invalid']="Указан недействительный адрес электронной почты.";
+$plugin_tx['register']['err_email_exists']="Пользователь с таким электронным адресом уже зарегистрирован.";
+$plugin_tx['register']['err_email_does_not_exist']="Пользователь с таким электронным адресом не существует.";
+$plugin_tx['register']['err_validation']="Неверный подтверждающий код.";
+$plugin_tx['register']['err_colon']="не может содержать двоеточие.";
+$plugin_tx['register']['err_group_does_not_exist']="Такой группы нет.";
+$plugin_tx['register']['err_csv_missing']="Файл CSV не существует.";
+$plugin_tx['register']['err_cannot_write_csv']="Файл CSV не сохранен.";
+$plugin_tx['register']['err_status_empty']="Ваша строка статуса не заполнена.";
+$plugin_tx['register']['err_status_invalid']="Введен неправильный код подтверждения.";
+$plugin_tx['register']['err_group_illegal']="Имя группы должно содержать только символы: <b>A-Z</b>, <b>a-z</b>, <b>0-9</b>, <b>_</b> .";
+$plugin_tx['register']['registered']="Вы зарегистрированы. На ваш электронный адрес отправлено сообщение, содержащее ссылку для активации вашей новой учетной записи.";
+$plugin_tx['register']['register_form1']="Новый пользователь получает статус <b>&quot;Гость&quot;</b> и имеет те же права, что и прежде. Статус может быть изменен администратором.";
+$plugin_tx['register']['register_form2']="Для регистрации может понадобиться несколько секунд, пожалуйста, подождите сообщения о успешной регистрации.";
+$plugin_tx['register']['activated']="Ваша учетная запись активирована.";
+$plugin_tx['register']['emailsubject']="Учетная запись активирована для\r\n";
+$plugin_tx['register']['emailtext1']="Учетная запись пользователя, указавшего этот электронный адрес, зарегистрирована на основании данных:";
+$plugin_tx['register']['emailtext2']="Пожалуйста, для активации вашей учетной записи нажмите ссылку:";
+$plugin_tx['register']['reminderexplanation']="На этой странице вы можете указать ваш электронный адрес для получения сообщения с данными вашей учетной записи.";
+$plugin_tx['register']['reminderemailsubject']="Данные учетной записи пользователя\r\n";
+$plugin_tx['register']['remindersent']="Сообщение с вашими учетными данными отослано.";
+$plugin_tx['register']['changeexplanation']="На этой странице вы можете изменить данные вашей учетной записи. Для изменения данных нужен ваш старый пароль.";
+$plugin_tx['register']['prefsemailsubject']="Данные учетной записи изменены для\r\n";
+$plugin_tx['register']['prefsupdated']="Данные вашей учетной записи обновлены и отосланы на ваш электронный адрес.";
+$plugin_tx['register']['emailprefsupdated']="Данные вашей учетной записи обновлены.";
+$plugin_tx['register']['mnu_main']="Пользователь/Группа";
+$plugin_tx['register']['mnu_config']="Конфигурация плагина";
+$plugin_tx['register']['mnu_language']="Настройки языка";
+$plugin_tx['register']['mnu_group_admin']="Управление группами";
+$plugin_tx['register']['mnu_user_admin']="Управление пользователями";
+$plugin_tx['register']['entries_in_csv']="Файл CSV ";
+$plugin_tx['register']['csv_written']="Файл CSV сохранен.";
+$plugin_tx['register']['config_login_page']="";
+$plugin_tx['register']['config_usersfile']="plugins/register/data/users.csv";
+$plugin_tx['register']['config_groupsfile']="plugins/register/data/groups.csv";
+$plugin_tx['register']['cf_allowed_register']="<p><b>true</b> Разрешить регистрацию новых пользователей.</b></p>";
+$plugin_tx['register']['cf_captcha_mode']="<p><b>none</b> Не использовать капчу.</b></p><p><b>formula</b> Активировать капчу в виде математической формулы.</b></p><p><b>image</b> Активировать капчу на основе изображения.</b></p>";
+$plugin_tx['register']['cf_encrypt_password']="<p><b>true</b> Активировать шифрование пароля.</b></p>";
+$plugin_tx['register']['cf_group_activated']="<p>Статус новых пользователей (группа после регистрации и активации учетной записи).</p>";
+$plugin_tx['register']['cf_group_administrator']="<p>Группа с правами администрации.</p>";
+$plugin_tx['register']['cf_group_default']="<p>Статус новых пользователей (группа по умолчанию для новых пользователей после регистрации, но до активации учетной записи).</p>";
+$plugin_tx['register']['cf_hide_pages']="<p><b>true</b> Разрешить скрытие страниц, к которым пользователь не имеет доступ.</p>";
+$plugin_tx['register']['cf_logfile']="<p><b>true</b> Разрешить запись в файл протокола сведений о входе/выходе пользователей.</p>";
+$plugin_tx['register']['cf_login_all_subsites']="<p><b>true</b> Одновременно разрешить доступ к всем страницам на других языках и субсайтам.</p>";
+$plugin_tx['register']['cf_login_layout']="<p><b>horizontal</b> Создать горизонтальную форму входа, если не указано, будет создана вертикальная форма входа.</p>";
+$plugin_tx['register']['cf_remember_user']="<p><b>true</b> Показать чекбокс \"Запомнить\" внизу формы входа.</p>";
+$plugin_tx['register']['cf_senderemail']="<p>Электронный адрес администратора.</p>";
+$plugin_tx['register']['cf_config_login_page']="Имя страницы для перенаправления пользователя после регистрации.";
+$plugin_tx['register']['cf_config_usersfile']="Путь, относительно корневой папки CMSimple к файлу CSV с данными пользователей, например \"plugins/register/data/users.csv\".";
+$plugin_tx['register']['cf_config_groupsfile']="Путь, относительно корневой папки CMSimple к файлу CSV с данными груп, например, \"plugins/register/data/groups.csv\".";
+$plugin_tx['register']['down_link']="ВНИЗ <span style=\"font-size: 24px;\">&dArr;</span>";
+
+?>
