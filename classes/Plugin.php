569b700734fd6d09be512f8dc28c30eecc6af5a7
Christoph M. Becker <cmbecker69@gmx.de>
2021-08-22 01:12:55 +0200

    Extract SpecialPageController class

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -60,172 +60,39 @@ class Plugin
      */
     private static function handleImplicitPages()
     {
-        global $o, $su, $plugin_tx;
+        global $o, $su, $h, $plugin_cf, $plugin_tx;
 
         switch ($su) {
             case uenc($plugin_tx['register']['register']):
-                $o .= self::handleRegistrationPage();
+                $method = 'registrationPageAction';
                 break;
             case uenc($plugin_tx['register']['forgot_password']):
-                $o .= self::handlePasswordForgottenPage();
+                $method = 'passwordForgottenPageAction';
                 break;
             case uenc($plugin_tx['register']['user_prefs']):
-                $o .= self::handleUserPrefsPage();
+                $method = 'userPrefsPageAction';
                 break;
             case uenc($plugin_tx['register']['login_error']):
-                $o .= self::handleLoginErrorPage();
+                $method = 'loginErrorPageAction';
                 break;
             case uenc($plugin_tx['register']['loggedout']):
-                $o .= self::handleLogoutPage();
+                $method = 'logoutPageAction';
                 break;
             case uenc($plugin_tx['register']['loggedin']):
-                $o .= self::handleLoginPage();
+                $method = 'loginPageAction';
                 break;
             case uenc($plugin_tx['register']['access_error']):
-                $o .= self::handleAccessErrorPage();
+                $method = 'accessErrorPageAction';
                 break;
+            default:
+                $method = null;
         }
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleRegistrationPage()
-    {
-        global $title, $h, $plugin_cf, $plugin_tx;
-
-        if ($plugin_cf['register']['allowed_register'] && !in_array($plugin_tx['register']['register'], $h)) {
-            $title = XH_hsc($plugin_tx['register']['register']);
-            return self::preparePageView(
-                $plugin_tx['register']['register'],
-                $plugin_tx['register']['register_form1'],
-                registerUser()
-            );
+        if ($method !== null) {
+            $controller = new SpecialPageController($h, $plugin_cf['register'], $plugin_tx['register']);
+            ob_start();
+            $controller->{$method}();
+            $o .= (string) ob_get_clean();
         }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handlePasswordForgottenPage()
-    {
-        global $title, $h, $plugin_cf, $plugin_tx;
-
-        if ($plugin_cf['register']['password_forgotten'] && !in_array($plugin_tx['register']['forgot_password'], $h)) {
-            $title = XH_hsc($plugin_tx['register']['forgot_password']);
-            return self::preparePageView(
-                $plugin_tx['register']['forgot_password'],
-                $plugin_tx['register']['reminderexplanation'],
-                registerForgotPassword()
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleUserPrefsPage()
-    {
-        global $title, $h, $plugin_tx;
-
-        if (!in_array($plugin_tx['register']['user_prefs'], $h)) {
-            $title = XH_hsc($plugin_tx['register']['user_prefs']);
-            return self::preparePageView(
-                $plugin_tx['register']['user_prefs'],
-                $plugin_tx['register']['changeexplanation'],
-                registerUserPrefs()
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleLoginErrorPage()
-    {
-        global $title, $h, $plugin_tx;
-
-        header('HTTP/1.1 403 Forbidden');
-        if (!in_array($plugin_tx['register']['login_error'], $h)) {
-            $title = $plugin_tx['register']['login_error'];
-            return self::preparePageView(
-                $plugin_tx['register']['login_error'],
-                $plugin_tx['register']['login_error_text']
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleLogoutPage()
-    {
-        global $title, $h, $plugin_tx;
-
-        if (!in_array($plugin_tx['register']['loggedout'], $h)) {
-            $title = $plugin_tx['register']['loggedout'];
-            return self::preparePageView(
-                $plugin_tx['register']['loggedout'],
-                $plugin_tx['register']['loggedout_text']
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleLoginPage()
-    {
-        global $title, $h, $plugin_tx;
-
-        if (!in_array($plugin_tx['register']['loggedin'], $h)) {
-            $title = $plugin_tx['register']['loggedin'];
-            return self::preparePageView(
-                $plugin_tx['register']['loggedin'],
-                $plugin_tx['register']['loggedin_text']
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @return ?View
-     */
-    private static function handleAccessErrorPage()
-    {
-        global $title, $h, $plugin_tx;
-
-        header('HTTP/1.1 403 Forbidden');
-        if (!in_array($plugin_tx['register']['access_error'], $h)) {
-            $title = $plugin_tx['register']['access_error'];
-            return self::preparePageView(
-                $plugin_tx['register']['access_error'],
-                $plugin_tx['register']['access_error_text']
-            );
-        }
-        return null;
-    }
-
-    /**
-     * @param string $title
-     * @param string $intro
-     * @param string $more
-     * @return View
-     */
-    private static function preparePageView($title, $intro, $more = '')
-    {
-        $view = new View('page');
-        $view->setData([
-            'title' => $title,
-            'intro' => $intro,
-            'more' => new HtmlString($more),
-        ]);
-        return $view;
     }
 
     private static function isAdmin(): bool
---
+++ classes/SpecialPageController.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Copyright (c) 2007 Carsten Heinelt (http://cmsimple.heinelt.eu)
+ * Copyright (c) 2010-2012 Gert Ebersbach (http://www.ge-webdesign.de/cmsimpleplugins/)
+ * Copyright (c) 2012-2021 Christoph M. Becker
+ *
+ * This file is part of Register_XH.
+ */
+
+namespace Register;
+
+class SpecialPageController
+{
+    /** @var string[] */
+    private $headings;
+
+    /** @var array<string,string> */
+    private $conf;
+
+    /** @var array<string,string> */
+    private $text;
+
+    /**
+     * @param string[] $headings
+     * @param array<string,string> $conf
+     * @param array<string,string> $text
+     */
+    public function __construct(array $headings, array $conf, array $text)
+    {
+        $this->headings = $headings;
+        $this->conf = $conf;
+        $this->text = $text;
+    }
+
+    /**
+     * @return void
+     */
+    public function registrationPageAction()
+    {
+        global $title;
+
+        if ($this->conf['allowed_register'] && !in_array($this->text['register'], $this->headings)) {
+            $title = XH_hsc($this->text['register']);
+            $this->renderPageView(
+                $this->text['register'],
+                $this->text['register_form1'],
+                registerUser()
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function passwordForgottenPageAction()
+    {
+        global $title;
+
+        if ($this->conf['password_forgotten'] && !in_array($this->text['forgot_password'], $this->headings)) {
+            $title = XH_hsc($this->text['forgot_password']);
+            $this->renderPageView(
+                $this->text['forgot_password'],
+                $this->text['reminderexplanation'],
+                registerForgotPassword()
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function userPrefsPageAction()
+    {
+        global $title;
+
+        if (!in_array($this->text['user_prefs'], $this->headings)) {
+            $title = XH_hsc($this->text['user_prefs']);
+            $this->renderPageView(
+                $this->text['user_prefs'],
+                $this->text['changeexplanation'],
+                registerUserPrefs()
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function loginErrorPageAction()
+    {
+        global $title;
+
+        header('HTTP/1.1 403 Forbidden');
+        if (!in_array($this->text['login_error'], $this->headings)) {
+            $title = $this->text['login_error'];
+            $this->renderPageView(
+                $this->text['login_error'],
+                $this->text['login_error_text']
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function logoutPageAction()
+    {
+        global $title;
+
+        if (!in_array($this->text['loggedout'], $this->headings)) {
+            $title = $this->text['loggedout'];
+            $this->renderPageView(
+                $this->text['loggedout'],
+                $this->text['loggedout_text']
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function loginPageAction()
+    {
+        global $title;
+
+        if (!in_array($this->text['loggedin'], $this->headings)) {
+            $title = $this->text['loggedin'];
+            $this->renderPageView(
+                $this->text['loggedin'],
+                $this->text['loggedin_text']
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function accessErrorPageAction()
+    {
+        global $title;
+
+        header('HTTP/1.1 403 Forbidden');
+        if (!in_array($this->text['access_error'], $this->headings)) {
+            $title = $this->text['access_error'];
+            $this->renderPageView(
+                $this->text['access_error'],
+                $this->text['access_error_text']
+            );
+        }
+    }
+
+    /**
+     * @param string $title
+     * @param string $intro
+     * @param string $more
+     * @return void
+     */
+    private function renderPageView($title, $intro, $more = '')
+    {
+        $view = new View('page');
+        $view->setData([
+            'title' => $title,
+            'intro' => $intro,
+            'more' => new HtmlString($more),
+        ]);
+        $view->render();
+    }
+}
