5b9b335f9491033407ad3102fda992c9b8577a54
Christoph M. Becker <cmbecker69@gmx.de>
2021-09-25 16:29:31 +0200

    Simplify error handling in RegistrationController
    Instead of accumulating error messages, we bail out on error.

--- classes/RegistrationController.php
+++ classes/RegistrationController.php
@@ -68,7 +68,6 @@ class RegistrationController
          */
         global $su;
 
-        // $errors = [];
         $name      = isset($_POST['name']) && is_string($_POST["name"]) ? trim($_POST['name']) : '';
         $username  = isset($_POST['username']) && is_string($_POST["username"]) ? trim($_POST['username']) : '';
         $password1 = isset($_POST['password1']) && is_string($_POST["password1"]) ? trim($_POST['password1']) : '';
@@ -77,6 +76,10 @@ class RegistrationController
 
         $validationService = new ValidationService($this->lang);
         $errors = $validationService->validateUser($name, $username, $password1, $password2, $email);
+        if ($errors) {
+            echo $this->view->render('error', ['errors' => $errors]);
+            echo $this->form($name, $username, $password1, $password2, $email);
+        }
 
         // read user file in CSV format separated by colons
         $this->dbService->lock(LOCK_EX);
@@ -84,7 +87,9 @@ class RegistrationController
 
         // check if user or other user for same email address exists
         if (registerSearchUserArray($userArray, 'username', $username) !== false) {
-            $errors[] = $this->lang['err_username_exists'];
+            $this->view->message("fail", $this->lang['err_username_exists']);
+            $this->dbService->lock(LOCK_UN);
+            return;
         }
         $user = registerSearchUserArray($userArray, 'email', $email);
 
@@ -101,43 +106,40 @@ class RegistrationController
         );
 
         // write CSV file if no errors occurred so far
-        if (empty($errors) && !$user && !$this->dbService->writeUsers($userArray)) {
-            $errors[] = $this->lang['err_cannot_write_csv'];
+        if (!$user && !$this->dbService->writeUsers($userArray)) {
+            $this->view->message("fail", $this->lang['err_cannot_write_csv']);
+            $this->dbService->lock(LOCK_UN);
+            return;
         }
         $this->dbService->lock(LOCK_UN);
 
-        if (!empty($errors)) {
-            echo $this->view->render('error', ['errors' => $errors]);
-            echo $this->form($name, $username, $password1, $password2, $email);
+        // prepare email content for registration activation
+        $content = $this->lang['emailtext1'] . "\n\n"
+            . ' ' . $this->lang['name'] . ": $name \n"
+            . ' ' . $this->lang['username'] . ": $username \n"
+            . ' ' . $this->lang['email'] . ": $email \n"
+            . ' ' . $this->lang['fromip'] . ": {$_SERVER['REMOTE_ADDR']} \n\n";
+        if (!$user) {
+            $content .= $this->lang['emailtext2'] . "\n\n"
+                . '<' . CMSIMPLE_URL . '?' . $su . '&'
+                . 'action=register_activate_user&username='.$username.'&nonce='
+                . $status . '>';
         } else {
-            // prepare email content for registration activation
-            $content = $this->lang['emailtext1'] . "\n\n"
-                . ' ' . $this->lang['name'] . ": $name \n"
-                . ' ' . $this->lang['username'] . ": $username \n"
-                . ' ' . $this->lang['email'] . ": $email \n"
-                . ' ' . $this->lang['fromip'] . ": {$_SERVER['REMOTE_ADDR']} \n\n";
-            if (!$user) {
-                $content .= $this->lang['emailtext2'] . "\n\n"
-                    . '<' . CMSIMPLE_URL . '?' . $su . '&'
-                    . 'action=register_activate_user&username='.$username.'&nonce='
-                    . $status . '>';
-            } else {
-                $content .= $this->lang['emailtext4'] . "\n\n"
-                    . '<' . CMSIMPLE_URL . '?' . uenc($this->lang['forgot_password']) . '>';
-            }
-
-            // send activation email
-            $this->mailService->sendMail(
-                $email,
-                $this->lang['emailsubject'] . ' ' . $_SERVER['SERVER_NAME'],
-                $content,
-                array(
-                    'From: ' . $this->config['senderemail'],
-                    'Cc: '  . $this->config['senderemail']
-                )
-            );
-            echo $this->view->message('success', $this->lang['registered']);
+            $content .= $this->lang['emailtext4'] . "\n\n"
+                . '<' . CMSIMPLE_URL . '?' . uenc($this->lang['forgot_password']) . '>';
         }
+
+        // send activation email
+        $this->mailService->sendMail(
+            $email,
+            $this->lang['emailsubject'] . ' ' . $_SERVER['SERVER_NAME'],
+            $content,
+            array(
+                'From: ' . $this->config['senderemail'],
+                'Cc: '  . $this->config['senderemail']
+            )
+        );
+        echo $this->view->message('success', $this->lang['registered']);
     }
 
     /**
@@ -161,9 +163,6 @@ class RegistrationController
 
     private function activateUser(string $user, string $nonce): string
     {
-        $errors = [];
-        $o ='';
-
         // read user file in CSV format separated by colons
         $this->dbService->lock(LOCK_EX);
         $userArray = $this->dbService->readUsers();
@@ -171,25 +170,26 @@ class RegistrationController
         // check if user or other user for same email address exists
         $entry = registerSearchUserArray($userArray, 'username', $user);
         if ($entry === false) {
-            $errors[] = $this->lang['err_username_notfound'] . $user;
-        } else {
-            if ($entry->getStatus() == "") {
-                $errors[] = $this->lang['err_status_empty'];
-            }
-            if ($nonce != $entry->getStatus()) {
-                $errors[] = $this->lang['err_status_invalid'];
-            }
+            $this->view->message("fail", $this->lang['err_username_notfound'] . $user);
+            $this->dbService->lock(LOCK_UN);
+            return "";
+        }
+        if ($entry->getStatus() == "") {
+            $this->view->message("fail", $this->lang['err_status_empty']);
+            $this->dbService->lock(LOCK_UN);
+            return "";
+        }
+        if ($nonce != $entry->getStatus()) {
+            $this->view->message("fail", $this->lang['err_status_invalid']);
+            $this->dbService->lock(LOCK_UN);
+            return "";
         }
 
-        if (!empty($errors)) {
-            $o .= $this->view->render('error', ['errors' => $errors]);
-        } else {
-            $entry->activate();
-            $entry->setAccessgroups(array($this->config['group_activated']));
-            $userArray = registerReplaceUserEntry($userArray, $entry);
-            $this->dbService->writeUsers($userArray);
-            $o .= $this->view->message('success', $this->lang['activated']);
-        }
+        $entry->activate();
+        $entry->setAccessgroups(array($this->config['group_activated']));
+        $userArray = registerReplaceUserEntry($userArray, $entry);
+        $this->dbService->writeUsers($userArray);
+        $o = $this->view->message('success', $this->lang['activated']);
         $this->dbService->lock(LOCK_UN);
         return $o;
     }
