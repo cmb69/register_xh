29c588bbf23c67c577eced577a2be3edc6816147
Christoph M. Becker <cmbecker69@gmx.de>
2023-01-29 12:18:40 +0100

    Test MainAdminController::saveGroupsAction()

--- tests/unit/MainAdminControllerTest.php
+++ tests/unit/MainAdminControllerTest.php
@@ -17,32 +17,129 @@ class MainAdminControllerTest extends TestCase
 {
     public function testEditGroupActionRendersGroups()
     {
-        $plugin_cf = XH_includeVar("./config/config.php", 'plugin_cf');
-        $conf = $plugin_cf['register'];
-        $plugin_tx = XH_includeVar("./languages/en.php", 'plugin_tx');
-        $lang = $plugin_tx['register'];
-        $csrfProtector = $this->createStub(CsrfProtector::class);
         $dbService = $this->createStub(DbService::class);
         $dbService->method('hasGroupsFile')->willReturn(true);
         $dbService->method('readGroups')->willReturn([new UserGroup("users", "")]);
-        $sut = new MainAdminController("./", $conf, $lang, $csrfProtector, $dbService, "/", $this->makePages());
+        $sut = $this->makeMainAdminController($dbService);
         $response = $sut->editGroupsAction();
         Approvals::verifyHtml($response);
     }
 
     public function testEditGroupActionFailsIfNoGroupsFile()
     {
-        $plugin_cf = XH_includeVar("./config/config.php", 'plugin_cf');
-        $conf = $plugin_cf['register'];
-        $plugin_tx = XH_includeVar("./languages/en.php", 'plugin_tx');
-        $lang = $plugin_tx['register'];
-        $csrfProtector = $this->createStub(CsrfProtector::class);
         $dbService = $this->createStub(DbService::class);
         $dbService->method('hasGroupsFile')->willReturn(false);
-        $pages = $this->createStub(Pages::class);
-        $sut = new MainAdminController("./", $conf, $lang, $csrfProtector, $dbService, "/", $pages);
+        $sut = $this->makeMainAdminController($dbService);
         $response = $sut->editGroupsAction();
         Approvals::verifyHtml($response);
+    }
+
+    public function testSaveGroupsCanAddRecord()
+    {
+        $_POST = [
+            "add" => [""],
+            "groupname" => ["guest"],
+            "grouploginpage" => [""],
+
+        ];
+        $dbService = $this->createStub(DbService::class);
+        $dbService->expects($this->never())->method('writeGroups');
+        $sut = $this->makeMainAdminController($dbService);
+        $response = $sut->saveGroupsAction();
+        Approvals::verifyHtml($response);
+    }
+
+    public function testSaveGroupsCanDeleteRecord()
+    {
+        $_POST = [
+            "delete" => [0 => "1"],
+            "groupname" => ["to_be_deleted", "guest"],
+            "grouploginpage" => ["", "Start"],
+
+        ];
+        $dbService = $this->createStub(DbService::class);
+        $dbService->expects($this->never())->method('writeGroups');
+        $sut = $this->makeMainAdminController($dbService);
+        $response = $sut->saveGroupsAction();
+        Approvals::verifyHtml($response);
+    }
+
+    public function testSaveGroupsFailsOnInvalidGroupName()
+    {
+        $_POST = [
+            "groupname" => ["illegal name", "guest"],
+            "grouploginpage" => ["", "Start"],
+
+        ];
+        $dbService = $this->createStub(DbService::class);
+        $dbService->expects($this->never())->method('writeGroups');
+        $sut = $this->makeMainAdminController($dbService);
+        $response = $sut->saveGroupsAction();
+        Approvals::verifyHtml($response);
+    }
+
+    public function testSaveGroupsSuccessfullySaves()
+    {
+        $_POST = [
+            "groupname" => ["admin", "guest"],
+            "grouploginpage" => ["", "Start"],
+
+        ];
+        $dbService = $this->createStub(DbService::class);
+        $dbService->expects($this->once())->method('writeGroups')->willReturn(true);
+        $sut = $this->makeMainAdminController($dbService);
+        $response = $sut->saveGroupsAction();
+        Approvals::verifyHtml($response);
+    }
+
+    public function testSaveGroupsFailsToSaves()
+    {
+        $_POST = [
+            "groupname" => ["admin", "guest"],
+            "grouploginpage" => ["", "Start"],
+
+        ];
+        $dbService = $this->createStub(DbService::class);
+        $dbService->expects($this->once())->method('writeGroups')->willReturn(false);
+        $sut = $this->makeMainAdminController($dbService);
+        $response = $sut->saveGroupsAction();
+        Approvals::verifyHtml($response);
+    }
+
+    private function makeMainAdminController(DbService $dbService): MainAdminController
+    {
+        return new MainAdminController(
+            "./",
+            $this->makeConf(),
+            $this->makeLang(),
+            $this->makeCsrfProtector(false),
+            $dbService,
+            "/",
+            $this->makePages()
+        );
+    }
+
+    /** @return array<string,string> */
+    private function makeConf(): array
+    {
+        $plugin_cf = XH_includeVar("./config/config.php", 'plugin_cf');
+        return $plugin_cf['register'];
+    }
+
+    /** @return array<string,string> */
+    private function makeLang(): array
+    {
+        $plugin_tx = XH_includeVar("./languages/en.php", 'plugin_tx');
+        return $plugin_tx['register'];
+    }
+
+    private function makeCsrfProtector(bool $used): CsrfProtector
+    {
+        $csrfProtector = $this->createStub(CsrfProtector::class);
+        if ($used) {
+            $csrfProtector->expects($this->once())->method('check');
+        }
+        return $csrfProtector;
     }
 
     private function makePages(): Pages
