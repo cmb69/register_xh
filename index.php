ac953269021dda2488550dc54c5ee0af7fa7ddad
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-14 16:19:42 +0100

    Move user function from required_classes.php to index.php

--- index.php
+++ index.php
@@ -11,3 +11,55 @@
 use Register\Plugin;
 
 Plugin::run();
+
+/**
+ * Create and handle register form
+ */
+function registerUser(): string
+{
+    return Plugin::handleUserRegistration();
+}
+
+/**
+ * Create and handle forgotten password form
+ */
+function registerForgotPassword(): string
+{
+    return Plugin::handleForgotPassword();
+}
+
+/*
+ * Create and handle user preferences form
+ */
+function registerUserPrefs(): string
+{
+    return Plugin::handleUserPrefs();
+}
+
+/*
+ * Show the login or logged in form
+ */
+function registerloginform(): string
+{
+    return Plugin::handleLoginForm();
+}
+
+/**
+ * Show the logged in form, if user is logged in
+ *
+ * @since 1.5rc1
+ */
+function Register_loggedInForm(): string
+{
+    return Plugin::handleloggedInForm();
+}
+
+function register_access(string $groupString): string
+{
+    return Plugin::handlePageAccess($groupString);
+}
+
+function access(string $groupString): string
+{
+    return Plugin::handlePageAccess($groupString);
+}
